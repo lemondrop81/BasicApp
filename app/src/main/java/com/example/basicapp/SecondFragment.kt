882614a6df42008eca8e7c4e4e10afb14608abd8
lemondrop81 <hacker814@hotmail.com>
2021-11-01 08:36:58 -0500

    Display second fragment

--- app/src/main/java/com/example/basicapp/SecondFragment.kt
+++ app/src/main/java/com/example/basicapp/SecondFragment.kt
@@ -1,6 +1,7 @@
 package com.example.basicapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,6 +24,8 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        Log.d(TAG, "onCreateView: Second fragment")
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
